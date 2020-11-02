d21f84644780bfbbc5e35141ac32384aeb61d10d
Kelin Field <kfield111@hotmail.com>
2020-11-02 06:35:10 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,20 @@ class User < ActiveRecord::Base
   has_many :tweets
 
   def slug
-   @slug = slugify(self.username)
-end
+    @slug = slugify(self.username)
+  end
 
-def slugify(name)
+  def slugify(name)
     split_on_apostrophes = name.split(/[']/)
     name_without_apost = split_on_apostrophes.join
     name_array = name_without_apost.downcase.split(/[\W]/)
     name_array.delete_if{|x|x==""}
     new_name = name_array.join("-")
-end
+  end
 
-def self.find_by_slug(slug)
-  self.all.detect{|x|x.slug == slug}
-end
+  def self.find_by_slug(slug)
+    self.all.detect{|x|x.slug == slug}
+  end
 
 
 end
