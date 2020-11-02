29c0397f7f7a6ba5cc131d5aaaace97f0db60f4b
Kelin Field <kfield111@hotmail.com>
2020-11-02 05:53:08 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,9 +3,6 @@ class TweetsController < ApplicationController
   get '/tweets' do
         @tweets = Tweet.all
         erb :'/tweets/show'
-      else
-        redirect '/users/login'
-      end
     end
 
 end
