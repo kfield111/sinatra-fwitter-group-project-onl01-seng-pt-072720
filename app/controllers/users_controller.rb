2f114fb79a32c264f27f8d7cfa4937b2a7a10b94
Kelin Field <kfield111@hotmail.com>
2020-11-02 05:17:53 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,4 +31,15 @@ class UsersController < ApplicationController
     redirect '/tweets'
   end
 
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
+
 end
