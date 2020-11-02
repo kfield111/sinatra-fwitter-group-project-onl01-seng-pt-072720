bf40cec3019ae55bc3411d2d4459834e14359fde
Kelin Field <kfield111@hotmail.com>
2020-11-02 05:02:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,10 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    if logged_in?
+    user =  User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect '/tweets'
     else
       erb :'/registration/signup'
