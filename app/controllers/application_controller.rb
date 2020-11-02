9515b48fea5a9cdfcb4fbca8c4b5a42e0a84260a
Kelin Field <kfield111@hotmail.com>
2020-11-02 03:53:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-    session[:user_id] = user.id
     if user.save && user.username != "" && user.email != ""
+      user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      session[:user_id] = user.id
       redirect '/tweets'
     else
       redirect '/signup'
