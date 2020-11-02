8b4ccc74bf015a27a9340aaa439b34cb0dc5d0e2
Kelin Field <kfield111@hotmail.com>
2020-11-02 05:20:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,11 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-      @current_user = User.find(session[:user_id])
+      User.find(session[:user_id])
     end
 
     def logged_in?
-      @logged_in = !!session[:user_id]
+      !!session[:user_id]
     end
   end
 
