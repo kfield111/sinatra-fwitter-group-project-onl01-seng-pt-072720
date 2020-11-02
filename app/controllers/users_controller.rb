9ff6e62f81647254e49ec1a419223d820dabc5ec
Kelin Field <kfield111@hotmail.com>
2020-11-02 05:09:46 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/registration/signup'
+    end
+  end
+
+  post '/signup' do
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if user.save && user.username != "" && user.email != ""
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect '/signup'
+    end
+  end
+
 
   get '/login' do
-    erb :login
+    erb '/user/login'
   end
 
 end
