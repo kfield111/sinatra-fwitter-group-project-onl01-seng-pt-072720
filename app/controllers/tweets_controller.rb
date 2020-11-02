bf5fec0305a72ef96a6aacf640f4b421f20d688f
Kelin Field <kfield111@hotmail.com>
2020-11-02 03:25:51 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,11 @@
 class TweetsController < ApplicationController
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect '/failure'
-    else
+    # if params[:username] == "" || params[:email] == "" || params[:password] == ""
+    #   redirect '/failure'
+    # else
       @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-      redirect '/tweets'
+      redirect '/tweets/index'
     end
   end
 
