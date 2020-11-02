3f9e51ee9f2cb8d72cd2fa915215e0c536e39377
Kelin Field <kfield111@hotmail.com>
2020-11-02 04:08:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2020_11_02_024433) do
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_idend"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
 end
