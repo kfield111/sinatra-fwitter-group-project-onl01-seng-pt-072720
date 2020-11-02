1f7113ab676576785702bf0b788d86d101cd3d42
Kelin Field <kfield111@hotmail.com>
2020-11-02 07:13:37 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     if is_logged_in?
       redirect '/tweets'
     else
-      erb :'/users/signup'
+      erb :'/registration/signup'
     end
   end
 
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     end
     redirect to "/tweets"
   end
+
 
   get '/login' do
     if is_logged_in?
@@ -36,14 +37,14 @@ class UsersController < ApplicationController
     redirect '/tweets'
   end
 
+  get '/logout' do
+      session.clear
+      redirect "/login"
+  end
+
   get "/users/:slug" do
     @user = User.find_by_slug(params[:slug])
     erb :'/users/show'
-  end
-
-  get '/logout' do
-    session.clear
-    redirect "/login"
   end
 
 end
