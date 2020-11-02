3b8719044edb46efb121c34fe69a5ba375318a88
Kelin Field <kfield111@hotmail.com>
2020-11-02 06:25:25 +0000

    Automatically backed up by Learn

--- db/migrate/20201102024433_create_tweets.rb
+++ db/migrate/20201102024433_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table  :tweets do |t|
       t.text :content
-      t.integer :user_idend
-    end      
+      t.integer :user_id
+    end
   end
 end
