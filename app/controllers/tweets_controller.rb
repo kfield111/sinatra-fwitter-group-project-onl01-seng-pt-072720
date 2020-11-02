905f031e649574a0745dc61e45883bef1642eab4
Kelin Field <kfield111@hotmail.com>
2020-11-02 05:03:48 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-      @user = User.find(session[:user_id])
+      # @user = User.find(session[:user_id])
       @tweets = Tweet.all
       erb :'/tweets/show'
   end
