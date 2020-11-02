fe147bb3f3f14e5a67f5ddd8752a45b6e8b4bc16
Kelin Field <kfield111@hotmail.com>
2020-11-02 06:15:38 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
     if logged_in?
       @user = User.find(session[:user_id])
       @tweets = Tweet.all
-      erb :'/users/show'
+      erb :'/tweets/show'
     else
      redirect '/login'
     end
